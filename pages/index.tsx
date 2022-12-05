6fa03902b0beed9d5ba99732f709d64780ea7283
Hachiman12a1 <daovandat2k15@gmail.com>
2022-12-05 16:19:17 +0700

    Display Posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import { Categories, PostCard, PostWidget } from "../components";
 import { getPosts } from "../services/index";
 
 type PageProps = {
-  posts: [];
+  posts: any[];
 };
 
 const Home: NextPage<PageProps> = ({ posts }) => {
@@ -18,7 +18,7 @@ const Home: NextPage<PageProps> = ({ posts }) => {
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post) => (
-            <PostCard post={post} key={post.title} />
+            <PostCard post={post.node} key={post.title} />
           ))}
         </div>
         <div className="lg:col-span-4 col-span-1">
