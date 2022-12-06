6fe1f0c0420fecaa2ddb616431577c27647ad251
Hachiman12a1 <daovandat2k15@gmail.com>
2022-12-06 10:47:45 +0700

    Widget Component

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,7 +23,7 @@ const Home: NextPage<PageProps> = ({ posts }) => {
         </div>
         <div className="lg:col-span-4 col-span-1">
           <div className="lg:sticky relative top-8">
-            <PostWidget />
+            <PostWidget slug={undefined} />
             <Categories />
           </div>
         </div>
