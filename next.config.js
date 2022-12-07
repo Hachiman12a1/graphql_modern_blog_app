6988cf2ff4c82f10402bb2f70a45265c2c363ff0
Hachiman12a1 <daovandat2k15@gmail.com>
2022-12-07 10:03:43 +0700

    Author

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        hostname: "media.graphassets.com",
+      },
+    ],
+  },
 };
